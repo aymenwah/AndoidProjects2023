8f7040293cc9e81efc431211456ac2f4d43f6ddb
hossem <hammamihossem99@gmail.com>
2023-12-12 16:17:58 +0100

    up

--- Houssem_hammami_dsi31_TheMovizz/app/src/main/java/com/example/themovizz/Utils/MovieDetailsResultCallback.java
+++
@@ -1,9 +0,0 @@
-package com.example.themovizz.Utils;
-
-import com.example.themovizz.Api.models.SingleMovie;
-
-public interface MovieDetailsResultCallback {
-    void onMovieDetailsReceived(SingleMovie movie);
-
-    void onError(String errorMessage);
-}
